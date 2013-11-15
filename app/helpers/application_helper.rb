3a36617b794c5834e953c4b86371e1afbe5df12f
Rohan Prinja <rohan.prinja@gmail.com>
2013-11-16 00:59:25 +0530

    Added a fullscreen background image for the homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,19 @@
 module ApplicationHelper
-  def signed_in?
-    false
+  include SessionsHelper
+end
+
+# Monkeypatching
+
+class Time
+  # converts 2000-01-01 10:55:00 UTC => 10:55:00 UTC
+  # return is of class String
+  def remove_year
+    to_s.split[1,2].join(' ')
+  end
+end
+
+class Float
+  def round_point5
+    (self*2).round / 2.0
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,35 @@
 module SessionsHelper
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    # update the remember token
+    ActiveRecord::Base.connection.execute <<-SQL
+    update users set remember_token='#{User.encrypt(remember_token)}'
+    where username='#{user.username}';
+    SQL
+    self.current_user = user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    # set current_user to the user identified by the remember token, but only if the @current_user variable is undefined
+    @current_user ||= User.find_by(:remember_token => remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
 end
